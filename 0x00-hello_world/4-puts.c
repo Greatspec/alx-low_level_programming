23da89c2b3483bf8308dcb2837b634281243eb23
Greatspec <victorychibuezespec@gmail.com>
2022-07-05 05:13:48 -0700

    lovely c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase properly,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+
+
+}
